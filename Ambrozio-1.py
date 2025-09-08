d548df6850931f1984822256b453f3bf72bcff43
an0n-ambrozio <matheus@ambrozio.space>
2025-09-07 22:19:13 -0500

    #1 Problem Solved (09.07.2025)

---
+++ Ambrozio-1.py
@@ -0,0 +1,12 @@
+# The answer is: 233168
+
+result = 0
+
+for i in range(1000):
+    if i % 3 == 0 or i % 5 == 0:
+        result += i
+
+print("The sum of all multiples of 3 or 5 below 1000 is:", result)
+
+
+
